89bc7594b37f60f8fd7cf78ae8d0b6795d33b3dc
cuikaihua <cuikaihua@weshare.com.cn>
2019-07-29 23:43:52 +0800

    add asset management

--- Getting Start/src/index.js
+++ Getting Start/src/index.js
@@ -1,7 +1,17 @@
+import './style.css';
+import Icon from './icon.jpg';
+
 function component() {
     const element = document.createElement('div');
 
     element.innerHTML = ['hello', 'world'].join(' ');
+    element.classList.add('hello');
+
+    const myIcon = new Image();
+    myIcon.src = Icon;
+
+    element.appendChild(myIcon);
+
     return element;
 }
 
